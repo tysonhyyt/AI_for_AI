b4d5c5a897bb38a173d08034f4f23b0126640c7e
tysonhyyt <66477564+tysonhyyt@users.noreply.github.com>
2021-11-09 14:42:51 +0800

    Update barChart.py

--- barChart.py
+++ barChart.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import time
 import numpy as np
-import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 import io
 import base64
 
